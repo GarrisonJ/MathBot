139caaadd158859b7537fa13099e83af4dc858cf
Garrison Jensen <garrison.jensen@gmail.com>
2014-03-27 22:25:10 -0700

    New feature using the bang symbol

--- mathbot.rb
+++ mathbot.rb
@@ -3,7 +3,10 @@ require "socket"
 require "openssl"
 
 # Variables
-@help_msg     = "Use the following symbols in a message: 0-9, +, -, /, *, (, ). If it's a syntactically correct mathematical expression, I will try to evaluate it."
+@help_msg     = ["Use the following symbols in a message: 0-9, +, -, /, *, (, ), !.", \
+								"If it's a syntactically correct mathematical expression, I will try to evaluate it.",\
+								"Use '**' for exponentiation.",\
+								"The variable '!' holds the result of the previous calculation."]
 @server       = "irc.cat.pdx.edu"
 @port         = "6697"
 @nick         = "Math"
@@ -20,14 +23,27 @@ require "openssl"
 @irc_server.puts "NICK #{@nick}"
 @irc_server.puts "JOIN #{@channel} #{@channelPass}"
 
+@previous_value = 0
+
+def help_msg
+	@help_msg.each do |msg|
+      @irc_server.puts "PRIVMSG #{@channel} :" + msg
+	end
+end
+
+
 def well_formed_equation? (astring)
-  (astring.delete "1234567890+\-/*)(").chomp.length == 0 and
+  (astring.delete "!1234567890+\-/*)(").chomp.length == 0 and
   (astring.include? "+" or
    astring.include? "-" or
    astring.include? ")" or
    astring.include? "(" or
    astring.include? "*" or
    astring.include? "/")
+end
+
+def replace_special_characters! (astring)
+	astring.gsub! /!/,  @previous_value.to_s
 end
 
 def evaluate (calculation)
@@ -50,12 +66,14 @@ until @irc_server.eof? do
   result = nil
   if calc != nil 
     if calc.chomp == "Math help"
-      @irc_server.puts "PRIVMSG #{@channel} :" + @help_msg
+			help_msg
     else well_formed_equation? calc
+			replace_special_characters! calc
       result = evaluate calc
     end
   end
   if result != nil
+		@previous_value = result.to_s
     @irc_server.puts "PRIVMSG #{@channel} :" + result.to_s
   end
 end
